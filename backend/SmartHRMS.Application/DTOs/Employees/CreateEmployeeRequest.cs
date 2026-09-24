7e0eae6ccc3435571a83b5e844c34ada652b91b0
Aminul Islam Mahim <104668744+Aminulislammahim23@users.noreply.github.com>
2026-09-24 14:31:42 +0600

    Day 7 : feat(employee): add employee management and photo support

---
+++ backend/SmartHRMS.Application/DTOs/Employees/CreateEmployeeRequest.cs
@@ -0,0 +1,8 @@
+public class CreateEmployeeRequest{
+    public string EmployeeCode { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public DateTime JoiningDate { get; set; }
+}
